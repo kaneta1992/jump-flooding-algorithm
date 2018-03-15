15cf6eae2c3dc516feea609dcb430243c9aa4b4d
kaneta <kaneta1011@gmail.com>
2018-03-15 18:30:14 +0900

    load png image

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	_ "image/png"
+	"os"
+)
 
 func main() {
-	fmt.Println("go")
+	file, _ := os.Open("./test.png")
+	defer file.Close()
+
+	config, _, err := image.DecodeConfig(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(config.Width)
+	fmt.Println(config.Height)
 }
