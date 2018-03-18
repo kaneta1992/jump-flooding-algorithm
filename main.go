ff0648b64e293d1e806d91d0c59e4b1de2d48d0f
kaneta <kaneta1011@gmail.com>
2018-03-19 02:55:49 +0900

    output jfa image

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"image/png"
 	"os"
-)
 
-func rgba(r uint32, g uint32, b uint32, a uint32) (uint8, uint8, uint8, uint8) {
-	return uint8(r / 257), uint8(g / 257), uint8(b / 257), uint8(a / 257)
-}
+	"github.com/kaneta1992/jump-flooding-algorithm/src/util"
+)
 
 func main() {
 	file, _ := os.Open("./test.png")
@@ -25,26 +22,20 @@ func main() {
 	fmt.Println(bounds.Max.X)
 	fmt.Println(bounds.Max.Y)
 
+	swap := util.JumpFlooding(img)
 	img2 := image.NewRGBA(image.Rect(0, 0, bounds.Max.X, bounds.Max.Y))
-
 	for y := 0; y < bounds.Max.Y; y++ {
 		for x := 0; x < bounds.Max.X; x++ {
-			col := img.At(x, y)
-			r, g, b, a := rgba(col.RGBA())
-			if r == 255 && g == 255 && b == 255 {
-				a = 0
-			}
-			img2.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
+			point := swap.Get(x, y).Nearest
+			col := swap.Get(point.X, point.Y).Color
+			img2.Set(x, y, col)
 		}
 	}
-
 	out, _ := os.Create("out.png")
 	defer out.Close()
-
 	png.Encode(out, img2)
-	//util.JumpFlooding(img)
 }
 
 // TODO:
-// JFAで最近のピクセルを計算する
 // ピクセルまでの距離を計算する
+// 8*8のドットではよさげに見えるけど、それ以外は何かおかしい
