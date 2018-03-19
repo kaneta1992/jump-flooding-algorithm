23d58b45a2525e5c65995535870b5e0e940efead
kaneta <kaneta1011@gmail.com>
2018-03-20 01:29:28 +0900

    adjust max distance value and time measurement

--- src/util/jfa.go
+++ src/util/jfa.go
@@ -1,9 +1,11 @@
 package util
 
 import (
+	"fmt"
 	"image"
 	"math"
 	"sync"
+	"time"
 )
 
 func length2(x, y int) int {
@@ -12,7 +14,7 @@ func length2(x, y int) int {
 
 func searchNearestPixel(x, y int, swap *SwapBuffer, level int) {
 	step := int(math.Exp2(float64(level)))
-	minDistance := 999999
+	minDistance := 999999999
 	var nearestPoint image.Point
 	for ny := -1; ny <= 1; ny++ {
 		for nx := -1; nx <= 1; nx++ {
@@ -39,6 +41,8 @@ func JumpFlooding(img image.Image) *SwapBuffer {
 	swap.InitActiveBuffer(img)
 	swap.Swap()
 	maxLevel := int(math.Log2(float64(bounds.Max.X))) - 1
+	beforeAt := time.Now()
+
 	for level := maxLevel; level >= 0; level-- {
 		wg := &sync.WaitGroup{}
 		for y := 0; y < height; y++ {
@@ -53,5 +57,7 @@ func JumpFlooding(img image.Image) *SwapBuffer {
 		wg.Wait()
 		swap.Swap()
 	}
+	afterAt := time.Now()
+	fmt.Println(afterAt.Sub(beforeAt).Nanoseconds())
 	return swap
 }
