d4881af0d95a6dacb6374e8981775c39baf2fd8f
kaneta <kaneta1011@gmail.com>
2018-03-25 14:30:49 +0900

    multi test images

--- main.go
+++ main.go
@@ -10,25 +10,28 @@ import (
 )
 
 func main() {
-	file, _ := os.Open("./test.png")
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	if err != nil {
-		panic(err)
+	images := 4
+	for i := 0; i < images; i++ {
+		file, _ := os.Open(fmt.Sprintf("./testImage/test%d.png", i))
+		defer file.Close()
+		img, _, err := image.Decode(file)
+		if err != nil {
+			panic(err)
+		}
+		bounds := img.Bounds()
+		fmt.Println(bounds.Max.X)
+		fmt.Println(bounds.Max.Y)
+
+		jfa := util.NewJFA(img)
+		voronoiImage := jfa.CalcVoronol()
+		sdfImage := jfa.CalcSDF(8.0)
+
+		voronoiFile, _ := os.Create(fmt.Sprintf("./testImage/voronoi%d.png", i))
+		defer voronoiFile.Close()
+		png.Encode(voronoiFile, voronoiImage)
+
+		sdfFile, _ := os.Create(fmt.Sprintf("./testImage/sdf%d.png", i))
+		defer sdfFile.Close()
+		png.Encode(sdfFile, sdfImage)
 	}
-	bounds := img.Bounds()
-	fmt.Println(bounds.Max.X)
-	fmt.Println(bounds.Max.Y)
-
-	jfa := util.NewJFA(img)
-	voronoiImage := jfa.CalcVoronol()
-	sdfImage := jfa.CalcSDF(8.0)
-
-	voronoiFile, _ := os.Create("voronoi.png")
-	defer voronoiFile.Close()
-	png.Encode(voronoiFile, voronoiImage)
-
-	sdfFile, _ := os.Create("sdf.png")
-	defer sdfFile.Close()
-	png.Encode(sdfFile, sdfImage)
 }
